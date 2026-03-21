97571d002514fc2dd8753dae9beba580b8b86eba
Pawel Gnat <pawel_gnat@o2.pl>
2026-03-21 10:28:27 +0100

    test: add auth e2e test

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const user = { email: 'test@example.com', password: 'test' };
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send(user)
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(user.email);
+      });
+  });
+});
